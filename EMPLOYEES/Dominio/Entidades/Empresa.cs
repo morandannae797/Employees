1ad97f93f345908139750d9d5f2cf4b904531098
morandannae797 <152675504+morandannae797@users.noreply.github.com>
2026-04-20 18:34:44 -0600

    Add files via upload

---
+++ EMPLOYEES/Dominio/Entidades/Empresa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMPLOYEES.Dominio.Entidades;
+
+public partial class Empresa
+{
+    public List<int> IdEmpresa { get; set; } = null!;
+
+    public char Nombre { get; set; }
+
+    public bool Factura { get; set; }
+}
---
+++ EMPLOYEES/Dominio/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMPLOYEES.Dominio.Entidades;
+
+public partial class Usuario
+{
+    public List<int> IdEmp { get; set; } = null!;
+
+    public char Password { get; set; }
+
+    public char Correo { get; set; }
+
+    public virtual ICollection<Admin> Admins { get; set; } = new List<Admin>();
+
+    public virtual ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+}
