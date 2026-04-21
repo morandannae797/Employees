7f326cd5724e2b2fd3eedc8861ab7bce8d31c726
morandannae797 <152675504+morandannae797@users.noreply.github.com>
2026-04-21 16:42:42 -0600

    Add files via upload

---
+++ EMPLOYEES/Dominio/Entidades/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMPLOYEES.Dominio.Entidades;
+
+public partial class Usuario
+{
+    public int IdEmp { get; set; } 
+
+    public string Password { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+}
