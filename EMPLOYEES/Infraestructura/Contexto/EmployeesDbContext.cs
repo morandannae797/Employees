d54e7f09b964b117170ea0d1deb6ebe37c05f514
morandannae797 <152675504+morandannae797@users.noreply.github.com>
2026-04-21 16:43:29 -0600

    Update EmployeesDbContext.cs

--- EMPLOYEES/Infraestructura/Contexto/EmployeesDbContext.cs
+++ EMPLOYEES/Infraestructura/Contexto/EmployeesDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using EMPLOYEES.Dominio.Entidades;
 using Microsoft.EntityFrameworkCore;
@@ -24,80 +24,19 @@ public partial class EmployeesDbContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
- protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-    optionsBuilder.UseNpgsql("Host=localhost;Database=employees;Username=postgres;Password=LOL");
-}
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+   
+  
+protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Admin>(entity =>
-        {
-            entity.HasKey(e => new { e.IdEmp, e.IdEmpresa }).HasName("Admin_pkey");
-
-            entity.ToTable("Admin");
-
-            entity.Property(e => e.IdEmp).HasColumnName("ID_emp");
-            entity.Property(e => e.IdEmpresa).HasColumnName("ID_empresa");
-            entity.Property(e => e.Nombre)
-                .HasColumnType("char")
-                .HasColumnName("nombre");
-
-            entity.HasOne(d => d.IdEmpNavigation).WithMany(p => p.Admins)
-                .HasForeignKey(d => d.IdEmp)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("ID_emp");
-        });
-
-        modelBuilder.Entity<Empleado>(entity =>
-        {
-            entity.HasKey(e => new { e.IdEmp, e.IdEmpresa }).HasName("Empleado_ID");
-
-            entity.ToTable("Empleado");
-
-            entity.Property(e => e.IdEmp).HasColumnName("ID_emp");
-            entity.Property(e => e.IdEmpresa).HasColumnName("ID_empresa");
-            entity.Property(e => e.Nombre)
-                .HasColumnType("char")
-                .HasColumnName("nombre");
-
-            entity.HasOne(d => d.IdEmpNavigation).WithMany(p => p.Empleados)
-                .HasForeignKey(d => d.IdEmp)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("ID_emp");
-        });
-
-        modelBuilder.Entity<Empresa>(entity =>
-        {
-            entity.HasKey(e => e.IdEmpresa).HasName("Empresa_pkey");
-
-            entity.ToTable("Empresa");
-
-            entity.Property(e => e.IdEmpresa).HasColumnName("ID_empresa");
-            entity.Property(e => e.Factura).HasColumnName("factura");
-            entity.Property(e => e.Nombre)
-                .HasColumnType("char")
-                .HasColumnName("nombre");
-        });
-
-        modelBuilder.Entity<Usuario>(entity =>
-        {
-            entity.HasKey(e => e.IdEmp).HasName("Usuario_pkey");
-
-            entity.ToTable("Usuario");
-
-            entity.Property(e => e.IdEmp).HasColumnName("ID_emp");
-            entity.Property(e => e.Correo)
-                .HasColumnType("char")
-                .HasColumnName("correo");
-            entity.Property(e => e.Password)
-                .HasColumnType("char")
-                .HasColumnName("password");
-        });
+        base.OnModelCreating(modelBuilder);
+modelBuilder.ApplyConfiguration(new  FluentConfiguracion.Empresa_FluentConfiguration());
+modelBuilder.ApplyConfiguration(new  FluentConfiguracion.Empleado_FluentConfiguration());
+modelBuilder.ApplyConfiguration(new  FluentConfiguracion.Admin_FluentConfiguration());
+modelBuilder.ApplyConfiguration(new  FluentConfiguracion.Usuario_FluentConfiguration());
 
         OnModelCreatingPartial(modelBuilder);
     }
 
+
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
-
